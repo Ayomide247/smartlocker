6b2520dca47fdb284a4549b90b33ed8af1822d95
Ayomide <bayomide247@gmail.com>
2023-07-19 01:28:59 +0100

    made some changge

--- src/component/header/HeroContainer.jsx
+++ src/component/header/HeroContainer.jsx
@@ -1,98 +1,104 @@
-import React from 'react'
+import React from "react";
 import appstore from "../../assets/image/appStore.jpg";
 import google from "../../assets/image/google.jpg";
 import banner1 from "../../assets/image/banner1.jpg";
-import Navbar from '../Navbar';
+import Navbar from "../Navbar";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
-import { SLIDER_2, SLIDER_3 } from '../../assets/image';
+import { SLIDER_2, SLIDER_3 } from "../../assets/image";
 
 const HeroContainer = () => {
-    return (
-        <>
-            <Navbar />
-            <section className="my-[8rem] w-full text-center ">
-                <div className="justify-between px-5 text-center items-center md:grid md:grid-cols-2 md:px-28 md:text-start">
-                    <div>
-                        <div className="font-extrabold md:text-[4rem] text-[2rem] w-full"
-                        data-aos='slide-right'
-                        data-aos-duration="2000"
-                        >
-                            <h1 className="leading-[1.2]">Secured <br /> Simplified Logistics <br /> for <span className="text-[#FF6000]">Africa</span></h1>
-                        </div>
-                        <p className="md:w-[85%] tracking-wider my-12 text-[17px]" data-aos='slide-right'
-                        data-aos-duration="1000">
-                            SmartParcel provides individuals, businesses and government
-                            organizations with better logistics services prioritizing speed,
-                            security and convenience
-                        </p>
-                        <div className="flex gap-5"
-                            data-aos='fade-up'
-                            data-aos-duration="2000"
-                        >
-                            <img
-                                className="md:w-[170px] h-[50px] w-[150px]"
-                                src={appstore}
-                            ></img>
-                            <img
-                                className="md:w-[170px] h-[50px] w-[150px]"
-                                src={google}
-                            ></img>
-                        </div>
-                    </div>
-                    <div className="my-10"
-                        data-aos="fade-left"
-                        data-aos-duration="1000"
-                    >
-                        <Splide
-                            options={{
-                                rewind: true,
-                                width: 650,
-                                autoplay: true,
-                                // gap   : '1rem',
-                                arrows: false,
-                            }}
-                            hasTrack={false}
-                            // aria-label='...' 
-                            className='slider'>
-                            <SplideTrack>
-                                <SplideSlide>
-                                    <div className='slider-1'>
-                                        <img
-                                            className='md:w-[85%] md:float-right'
-                                            src={banner1}
-                                            alt='slider-1'
-                                        />
-                                    </div>
-                                </SplideSlide>
+  return (
+    <div>
+      <Navbar />
+      <section className="my-[8rem] w-full text-center ">
+        <div className="items-center justify-between px-5 text-center md:grid md:grid-cols-2 md:px-28 md:text-start">
+          <div>
+            <div
+              className="font-extrabold md:text-[4rem] text-[2rem] w-full"
+              data-aos="slide-right"
+              data-aos-duration="2000"
+            >
+              <h1 className="leading-[1.2]">
+                Secured <br /> Simplified Logistics <br /> for{" "}
+                <span className="text-[#FF6000]">Africa</span>
+              </h1>
+            </div>
+            <p
+              className="md:w-[85%] tracking-wider my-12 text-[17px]"
+              data-aos="slide-right"
+              data-aos-duration="1000"
+            >
+              SmartParcel provides individuals, businesses and government
+              organizations with better logistics services prioritizing speed,
+              security and convenience
+            </p>
+            <div
+              className="flex gap-5"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              <img
+                className="md:w-[170px] h-[50px] w-[150px]"
+                src={appstore}
+              ></img>
+              <img
+                className="md:w-[170px] h-[50px] w-[150px]"
+                src={google}
+              ></img>
+            </div>
+          </div>
+          <div className="my-10" data-aos="fade-left" data-aos-duration="1000">
+            <Splide
+              options={{
+                rewind: true,
+                width: 650,
+                autoplay: true,
+                // gap   : '1rem',
+                arrows: false,
+              }}
+              hasTrack={false}
+              // aria-label='...'
+              className="slider"
+            >
+              <SplideTrack>
+                <SplideSlide>
+                  <div className="slider-1">
+                    <img
+                      className="md:w-[85%] md:float-right"
+                      src={banner1}
+                      alt="slider-1"
+                    />
+                  </div>
+                </SplideSlide>
 
-                                <SplideSlide>
-                                    <div className='slider-1'>
-                                        <img
-                                            className='md:w-[85%] md:float-right'
-                                            src={SLIDER_2}
-                                            alt='slider-1'
-                                        />
-                                    </div>
-                                </SplideSlide>
+                <SplideSlide>
+                  <div className="slider-1">
+                    <img
+                      className="md:w-[85%] md:float-right"
+                      src={SLIDER_2}
+                      alt="slider-1"
+                    />
+                  </div>
+                </SplideSlide>
 
-                                <SplideSlide>
-                                    <div className='slider-1'>
-                                        <img
-                                            className='md:w-[85%] md:float-right'
-                                            src={SLIDER_3}
-                                            alt='slider-1'
-                                        />
-                                    </div>
-                                </SplideSlide>
-                            </SplideTrack>
-                        </Splide>
-                        {/* <img className="md:w-[70%] md:float-right" src={banner1}></img> */}
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+                <SplideSlide>
+                  <div className="slider-1">
+                    <img
+                      className="md:w-[85%] md:float-right"
+                      src={SLIDER_3}
+                      alt="slider-1"
+                    />
+                  </div>
+                </SplideSlide>
+              </SplideTrack>
+            </Splide>
+            {/* <img className="md:w-[70%] md:float-right" src={banner1}></img> */}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default HeroContainer
+export default HeroContainer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-
 import whiteAppStore from "../assets/image/whiteAppStore.png";
 import whiteGooglePlay from "../assets/image/whiteGooglePlay.png";
 
@@ -9,18 +8,13 @@ import phone2 from "../assets/image/phone2.png";
 import smartlogo from "../assets/image/smartparcel_green_2.png";
 
 import { useEffect, useMemo, useState } from "react";
-import {
-  BsFacebook,
-  BsInstagram,
-  BsLinkedin,
-  BsTwitter
-} from "react-icons/bs";
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 import { FiChevronLeft, FiChevronRight } from "../assets/icons";
 import { HeroContainer, SingleFaq, SingleService } from "../component";
 import { faqsData, servicesData } from "../utils/data";
 
 const Home = () => {
-  const faqsItem = useMemo(() => faqsData, [])
+  const faqsItem = useMemo(() => faqsData, []);
   const [faqs, setFaqs] = useState(faqsData);
   const containerStyle = {
     backgroundImage: `url(${bgbanner})`,
@@ -30,29 +24,26 @@ const Home = () => {
     backgroundImage: `url(${parcel})`,
   };
 
-
-  // update faqs isOpen property on click of handler 
+  // update faqs isOpen property on click of handler
   const handleFaqClick = (id) => {
     const updatedFaqs = faqs.map((item, i) => {
-      console.log(id)
+      console.log(id);
       if (item.id === id) {
         item.isOpen = !item.isOpen;
       } else {
         item.isOpen = false;
       }
-      return item
-    })
+      return item;
+    });
     setFaqs(updatedFaqs);
-  }
+  };
 
   useEffect(() => {
-    console.log(faqs)
-  }, [faqs])
-
+    console.log(faqs);
+  }, [faqs]);
 
   return (
-    <section className="p-0 m-0 text-slate-700">
-
+    <section className="p-0 m-0 overflow-x-hidden text-slate-700">
       <HeroContainer />
 
       <section
@@ -60,13 +51,16 @@ const Home = () => {
         className="w-full h-full md:px-24 py-10 mt-10 text-center bg-fixed md:bg-left bg-no-repeat bg-cover text-[#FF6000]"
       >
         <div className="justify-between gap-24 px-5 py-16 md:flex md:px-24 md:flex-cols-3">
-          <div className="py-16 bg-inherit"
+          <div
+            className="py-16 bg-inherit"
             data-aos="zoom-in"
-            data-aos-duration="2000">
+            data-aos-duration="2000"
+          >
             <img className="w-[65%] m-auto" src={phone}></img>
           </div>
 
-          <div className="md:w-[50%] m-auto text-center"
+          <div
+            className="md:w-[50%] m-auto text-center"
             data-aos="fade-up"
             data-aos-duration="3000"
           >
@@ -90,15 +84,16 @@ const Home = () => {
 
       <section className="bg-[#f1640cfb] px-5 text-[#ffffffff] py-10 md:px-28">
         <div className="justify-between md:flex md:flex-cols-2">
-          <div className="text-center mt-6">
-            <div className="flex justify-between items-center"
+          <div className="mt-6 text-center">
+            <div
+              className="flex items-center justify-between"
               data-aos="fade-up"
               data-aos-duration="1000"
             >
               <h1 className="font-extrabold md:text-[3rem] text-[2rem] md:text-start">
                 SmartParcel Logistics Services
               </h1>
-              <div className="hidden md:flex justify-between gap-6">
+              <div className="justify-between hidden gap-6 md:flex">
                 <span className="w-12 h-12 rounded-full border-[1.5px] boder-white grid place-content-center text-lg cursor-pointer">
                   <FiChevronLeft />
                 </span>
@@ -107,7 +102,8 @@ const Home = () => {
                 </span>
               </div>
             </div>
-            <p className="my-2 text-[16px] text-start leading-7 md:w-[50%]"
+            <p
+              className="my-2 text-[16px] text-start leading-7 md:w-[50%]"
               data-aos="fade-up"
               data-aos-duration="2000"
             >
@@ -117,44 +113,39 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <div className="flex gap-5 my-7 w-[100%] md:w-[1200px] overflow-x-scroll pb-16"
-        >
-          {
-            servicesData.map((data) => (
-              <SingleService key={data.id} data={data} />
-            ))
-          }
+        <div className="flex gap-5 my-7 w-[100%] md:w-[1200px] overflow-x-scroll pb-16">
+          {servicesData.map((data) => (
+            <SingleService key={data.id} data={data} />
+          ))}
         </div>
-
       </section>
 
       <section className=" bg-[#FF60001A] md:px-28 px-5 md:pb-32">
-        <h1 className="font-extrabold leading-10 pt-8 text-[1.5rem] md:text-[3rem] text-center md:text-start md:pt-20"
+        <h1
+          className="font-extrabold leading-10 pt-8 text-[1.5rem] md:text-[3rem] text-center md:text-start md:pt-20"
           data-aos="fade-up"
           data-aos-duration="2000"
         >
           Frequently asked questions
         </h1>
-        <div className="md:flex items-start">
-          <div className="md:w-[50%]"
+        <div className="items-start md:flex">
+          <div
+            className="md:w-[50%]"
             data-aos="fade-up"
             data-aos-duration="3000"
           >
-
-            {
-              faqs.map((data, index) => (
-                <SingleFaq
-                  key={data.id}
-                  faq={data}
-                  id={data.id}
-                  handleFaqClick={handleFaqClick}
-                />
-              ))
-            }
-
+            {faqs.map((data, index) => (
+              <SingleFaq
+                key={data.id}
+                faq={data}
+                id={data.id}
+                handleFaqClick={handleFaqClick}
+              />
+            ))}
           </div>
 
-          <div className=" w-[50%] mx-auto"
+          <div
+            className=" w-[50%] mx-auto"
             data-aos="slide-left"
             data-aos-duration="2000"
           >
@@ -165,17 +156,16 @@ const Home = () => {
 
       <section
         // style={containerStyle}
-        className="bg-gray-900 w-full h-full px-5 py-10 text-center bg-fixed bg-no-repeat bg-cover md:px-28 md:bg:-left backdrop-brightness-100"
+        className="w-full h-full px-5 py-10 text-center bg-fixed bg-gray-900 bg-no-repeat bg-cover md:px-28 md:bg:-left backdrop-brightness-100"
       >
-        <div data-aos="fade-up"
-              data-aos-duration="3000">
+        <div data-aos="fade-up" data-aos-duration="3000">
           <h1 className="font-extrabold capitalize text-xl text-center justify-center py-10 md:w-3/5 m-auto text-white md:text-[2.5rem] md:leading-[3.2rem]">
             Be the first to know about update and offers
           </h1>
           <h4 className="font-thin tracking-wider text-center justify-center md:w-1/2 m-auto text-white md:text-[1.5rem]">
             Join our mailing list
           </h4>
-          <form className="justify-between my-7 md:w-5/6 mx-auto pb-20">
+          <form className="justify-between pb-20 mx-auto my-7 md:w-5/6">
             <div className="md:flex md:gap-4">
               <input
                 className="p-2.5 md:p-4 md:w-[70%] w-full rounded-md text-[16px] outline-none"
@@ -187,11 +177,10 @@ const Home = () => {
             </div>
           </form>
         </div>
-
       </section>
 
       <footer className="bg-[#FF6000] md:px-24 px-5 py-16 text-white text-[16px]">
-        <div className="justify-between items-center text-center md:flex flex-cols-3 ">
+        <div className="items-center justify-between text-center md:flex flex-cols-3 ">
           <div className="md:w-3/6">
             <div className="pb-4">
               <img className="w-[50%]" src={smartlogo}></img>
@@ -224,7 +213,11 @@ const Home = () => {
 
         <div className="justify-between md:flex ">
           <div>
-            <p> &copy; {new Date().getFullYear()} SmartParcel || All Rights Reserved.</p>
+            <p>
+              {" "}
+              &copy; {new Date().getFullYear()} SmartParcel || All Rights
+              Reserved.
+            </p>
             <div className="flex my-5 mt-5 gap-7">
               <BsFacebook />
               <BsTwitter />
